09136e1d7ebd9cf693e5168a52873ba448f2e24d
julian.laue <julian.laue@l3s.de>
2023-06-16 11:17:31 +0200

    - Add Webinterface

--- src/tutorial/qa_over_pdf.py
+++ src/tutorial/qa_over_pdf.py
@@ -1,20 +1,24 @@
 from langchain.document_loaders import PyPDFLoader
-from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
+from openai.error import OpenAIError
 
 from langchain.chains import RetrievalQA, ConversationalRetrievalChain
 from langchain.indexes import VectorstoreIndexCreator
 from langchain.text_splitter import CharacterTextSplitter, TokenTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
+import streamlit as st
 
 import os
+
+def clear_submit():
+    st.session_state["submit"] = False
 
 # set openAI API key
 os.environ["OPENAI_API_KEY"] = "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"
 
 # load the document
-loader = PyPDFLoader('C:/Users/lange/Documents/PyCharmProjects/chatbot-fuer-die-handwerkskammer-hannover/data/FAQ.pdf')
+loader = PyPDFLoader('C:/Users/lange/Documents/PyCharmProjects/chatbot-fuer-die-handwerkskammer-hannover/data/HandwerkskammerBotInfo.pdf')
 faq = loader.load()
 
 # split the documents into chunks
@@ -28,22 +32,33 @@ embeddings = OpenAIEmbeddings()
 db = Chroma.from_documents(texts, embeddings)
 
 # expose this index in a retriever interface
-retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":1})
+retriever = db.as_retriever(search_type="similarity", search_kwargs={"k":2})
 
 # create a chain to answer questions
 qa = ConversationalRetrievalChain.from_llm(
-    llm=OpenAI(), chain_type="stuff", retriever=retriever, return_source_documents=False)
+    llm=OpenAI(model_name='gpt-4'), chain_type="stuff", retriever=retriever, return_source_documents=False)
 
 chat_history = []
 cont = True
-while cont:
-    query = input('Ihre Frage: ')
-    result = qa({'question': query, 'chat_history': chat_history})
-    print('Antwort: ' + result['answer'])
-    chat_history.append((query, result['answer']))
 
-    cont_s = input("Haben Sie noch eine Frage? (y/n)")
-    if cont_s == 'n':
-        cont = False
+st.set_page_config(page_title="HandwerksGPT", page_icon="🔧", layout="wide")
+st.header("🔧Fragen an die Handwerkskammer")
+
+query = st.text_area("Stellen Sie Fragen an die Handwerkskammer zum Berufsausbildungsvertrag", on_change=clear_submit)
+
+button = st.button("Submit")
+
+if button or st.session_state.get("submit"):
+    st.session_state["submit"] = True
+    # Output Columns
+    answer_col = st.columns(1)
+    try:
+        answer = qa({'question': query, 'chat_history': chat_history})
+
+        st.markdown("#### Answer")
+        st.markdown(answer["answer"])
+
+    except OpenAIError as e:
+        st.error(e._message)
 
 
